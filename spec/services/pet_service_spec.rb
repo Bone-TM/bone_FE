99ec005c6990f12cf649f6bfc3f69b770ea9c109
Nick Jacobs <nickjacobsatc@gmail.com>
2022-07-27 18:19:57 -0500

    Test: gets pets

---
+++ spec/services/pet_service_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.describe "Pets Service API" do
+
+  it "can retrieve all pets from API" do
+    pets = PetService.get_pets
+    pet = pets.first
+
+    expect(pets).to be_a Array
+
+    expect(pet[:attributes][:name]).to be_a String
+    expect(pet[:attributes][:breed]).to be_a String
+    expect(pet[:attributes][:sex]).to be_a String
+    expect(pet[:attributes][:bio]).to be_a String
+    expect(pet[:attributes][:weight]).to be_a Integer
+    expect(pet[:attributes][:age]).to be_a Integer
+    expect(pet[:attributes][:user_id]).to be_a Integer
+  end
+
+end
