03a35fd891f10339618b1cba46c2c301f77bae88
Nick Jacobs <nickjacobsatc@gmail.com>
2022-07-30 16:52:56 -0500

    Feat: updated user to include pets

--- app/poros/user.rb
+++ app/poros/user.rb
@@ -1,5 +1,5 @@
 class User
-  attr_reader :name, :bio, :email, :location, :id
+  attr_reader :name, :bio, :email, :location, :id, :pets
 
   def initialize(user_data)
     @id = user_data[:id]
@@ -7,5 +7,14 @@ class User
     @bio = user_data[:attributes][:bio]
     @email = user_data[:attributes][:email]
     @location = user_data[:attributes][:location]
+    if !user_data[:attributes][:pets].nil?
+      @pets = user_pets(user_data)
+    end 
+  end
+
+  def user_pets(user_data)
+    user_data[:attributes][:pets].map do |pet_json|
+      Pet.new(pet_json)
+    end
   end
 end
