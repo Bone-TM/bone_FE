11898b51937fc9fdb3b407b83bf41a63e219d2a0
Nick Jacobs <nickjacobsatc@gmail.com>
2022-07-30 15:09:44 -0500

    Removed WhiteSpace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
     helper_method :current_user
-    
+
     def current_user
         User.find(session[:email]) if session[:email]
     end
+    
 end
