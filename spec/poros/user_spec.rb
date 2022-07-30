3c41b4e50c92e4a4f7a566939daaab527216e705
Nick Jacobs <nickjacobsatc@gmail.com>
2022-07-30 11:34:16 -0500

    Fix: adjusted spacing

--- spec/poros/user_spec.rb
+++ spec/poros/user_spec.rb
@@ -11,7 +11,6 @@ RSpec.describe User do
          :name=>"steve",
          :bio=>"dogs r kewl",
          :email=>"steve@beve.com",
-         :password_digest=>"sinoeingmeixnfrx88",
          :location=>"denver"
         }
       }
@@ -21,7 +20,6 @@ RSpec.describe User do
     expect(user.name).to eq(user_data[:attributes][:name])
     expect(user.bio).to eq(user_data[:attributes][:bio])
     expect(user.email).to eq(user_data[:attributes][:email])
-    expect(user.password_digest).to eq(user_data[:attributes][:password_digest])
     expect(user.location).to eq(user_data[:attributes][:location])
     expect(user.id).to eq(user_data[:id])
   end
