57d8e9b36d37a6375efbbfb5c8dca94440a5a847
Nick Jacobs <nickjacobsatc@gmail.com>
2022-07-30 15:09:57 -0500

    Removed commented method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,11 @@
 class SessionsController < ApplicationController
     def omniauth
-        # user = User.find_or_create_by(uid:request.env['omniauth.auth'][:uid], provider: request.env['omniauth.auth'][:provider]) do |u|
-        #     u.name = request.env['omniauth.auth'][:info][:name]
-        #     u.email = request.env['omniauth.auth'][:info][:email]
       oauth_response = request.env['omniauth.auth']
       user_data = {}
       user_data[:name] = oauth_response["info"]["name"]
       user_data[:email] = oauth_response["info"]["email"]
       UserService.create_user(user_data)
       session[:email] = user_data[:email]
-      redirect_to root_path
+      redirect_to users_dashboard_path
     end
   end
-#   end
-#     end
-#         if user.valid?
-#             session[:user_id] = user.id
-#             redirect_to root_path
-#         else
-#             redirect_to login_path
-#         end
-#     end
-# end
