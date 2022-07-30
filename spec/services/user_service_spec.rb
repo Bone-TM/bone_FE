05b813738685f966ac3e060be4363b61f66547c1
Nick Jacobs <nickjacobsatc@gmail.com>
2022-07-30 11:34:38 -0500

    Fix: added conditional if DB is empty

--- spec/services/user_service_spec.rb
+++ spec/services/user_service_spec.rb
@@ -4,16 +4,16 @@ RSpec.describe "Users API" do
 
   it "can retrieve all users from API" do
     users = UserService.get_users
-    user = users.first
-
-    expect(users).to be_a Array
     
-    expect(user[:attributes]).to include :name, :bio, :email, :password_digest, :location
-    expect(user[:attributes][:name]).to be_a String
-    expect(user[:attributes][:bio]).to be_a String
-    expect(user[:attributes][:email]).to be_a String
-    expect(user[:attributes][:password_digest]).to be_a String
-    expect(user[:attributes][:location]).to be_a String
-  end
+    if !users.nil?
+      user = users.first
+      expect(users).to be_a Array
 
+      expect(user[:attributes]).to include :name, :bio, :email, :location
+      expect(user[:attributes][:name]).to be_a String
+      expect(user[:attributes][:bio]).to be_a String
+      expect(user[:attributes][:email]).to be_a String
+      expect(user[:attributes][:location]).to be_a String
+    end
+  end
 end
