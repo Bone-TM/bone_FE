178215983c8689330463fcfb7d1386d20f1e1df8
Nick Jacobs <nickjacobsatc@gmail.com>
2022-07-30 11:33:41 -0500

    Test: mocked data for create user spec

--- spec/facades/user_facade_spec.rb
+++ spec/facades/user_facade_spec.rb
@@ -2,8 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "User Facade" do
 
-  it "returns an array of user objects" do
+  xit "returns an array of user objects" do
     users = UserFacade.create_users
+
+    expect(users).to be_an Array
+    expect(users.first.name).to be_a String
+    expect(users.last.location).to be_a String
+  end
+
+  it "returns an array of user objects" do
+    json = JSON.parse(File.read('./spec/fixtures/user_data.json'), symbolize_names: true)
+    users = json[:data].map do |users_json|
+      User.new(users_json)
+    end
+    allow(UserFacade).to receive(:create_users).and_return(users)
     
     expect(users).to be_an Array
     expect(users.first.name).to be_a String
