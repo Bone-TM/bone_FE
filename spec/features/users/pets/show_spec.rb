51f46cc9cbc93cfbca1512134beb11207af92463
Nick Jacobs <nickjacobsatc@gmail.com>
2022-07-30 16:55:16 -0500

    Test: shows details about specific pet

--- spec/features/users/pets/show_spec.rb
+++ spec/features/users/pets/show_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe "Pet Show Page" do
     @pet = Pet.new({id: 2, attributes: {name: "jimdog", bio: "Bork", weight: 15, age: 5, sex: "Male", breed: "beagle", user_id: @user.id}})
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     allow(PetFacade).to receive(:create_pet).and_return(@pet)
-
   end
 
   it "shows details about specific pet" do
