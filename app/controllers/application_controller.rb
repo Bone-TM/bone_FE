a94d2cb4cd0e4a69ba6ad0c4cc9d5c7ad6ee08f8
Nick Jacobs <nickjacobsatc@gmail.com>
2022-08-01 14:57:02 -0500

    refactor helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
     helper_method :current_user
 
     def current_user
-        User.find(session[:email]) if session[:email]
+        UserFacade.find_user(session[:email]) if session[:email]
     end
-    
+
 end
