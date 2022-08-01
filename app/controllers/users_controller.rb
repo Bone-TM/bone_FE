c519b8161684e98736e23d2a14c26084b2df5174
Nick Jacobs <nickjacobsatc@gmail.com>
2022-08-01 14:57:14 -0500

    Fix: adjusted new user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     user_data = {}
     user_data[:name] = oauth_response["info"]["name"]
     user_data[:email] = oauth_response["info"]["email"]
-    UserService.create_user(user_data)
+    response = UserService.create_user(user_data)
     session[:email] = user_data[:email]
     redirect_to users_dashboard_path
   end
