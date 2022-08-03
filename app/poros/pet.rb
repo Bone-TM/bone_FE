1f5e37715ba6694abf25079d3415d1674c8bf8a9
Saba Bhamidipati <saba.bhamidipati@gmail.com>
2022-08-03 14:24:31 -0400

    update pet controller for json formatting

--- app/poros/pet.rb
+++ app/poros/pet.rb
@@ -3,13 +3,22 @@ class Pet
 
   def initialize(pet_data)
     @id = pet_data[:id]
-    @name = pet_data[:attributes][:name]
-    @breed = pet_data[:attributes][:breed]
-    @sex = pet_data[:attributes][:sex]
-    @bio = pet_data[:attributes][:bio]
-    @weight = pet_data[:attributes][:weight]
-    @age = pet_data[:attributes][:age]
-    @user_id = pet_data[:attributes][:user_id]
+    if pet_data[:attributes]
+      @name = pet_data[:attributes][:name]
+      @breed = pet_data[:attributes][:breed]
+      @sex = pet_data[:attributes][:sex]
+      @bio = pet_data[:attributes][:bio]
+      @weight = pet_data[:attributes][:weight]
+      @age = pet_data[:attributes][:age]
+      @user_id = pet_data[:attributes][:user_id]
+    else 
+      @name = pet_data[:name]
+      @breed = pet_data[:breed]
+      @sex = pet_data[:sex]
+      @bio = pet_data[:bio]
+      @weight = pet_data[:weight]
+      @age = pet_data[:age]
+      @user_id = pet_data[:user_id]
+    end 
   end
-
 end
