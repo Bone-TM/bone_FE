1a23296fbb48ac31fc5e5b6b026a3b0febe12d9a
Zach Prince <zachprince@Zachs-MBP.hitronhub.home>
2022-08-03 14:46:04 -0400

    Remove debugger entry point

--- spec/services/geo_service_spec.rb
+++ spec/services/geo_service_spec.rb
@@ -3,8 +3,6 @@ require 'rails_helper'
 RSpec.describe 'Geolocation API' do
   it 'returns list of user location' do
     parsed_json = GeoService.get_location
-    require 'pry'
-    binding.pry
     expect(parsed_json).to be_a Hash
   end
 end
