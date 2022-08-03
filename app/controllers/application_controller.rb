1189d2617186fb4521cac554a840ea009c11769c
Zach Prince <zachprince@Zachs-MBP.hitronhub.home>
2022-08-03 14:41:39 -0400

    Add location logic to view and remove logic from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
-    helper_method :current_user
+  helper_method :current_user
 
-    def current_user
-        UserFacade.find_user(session[:email]) if session[:email]
-    end
-
+  def current_user
+    UserFacade.find_user(session[:email]) if session[:email]
+  end
 end
