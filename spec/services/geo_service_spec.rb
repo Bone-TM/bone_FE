b28d810b7c0206868ded5c8bb66493424f494cc7
Zach Prince <zachprince@Zachs-MBP.hitronhub.home>
2022-08-03 16:23:16 -0400

    Add geo facade test

--- spec/services/geo_service_spec.rb
+++ spec/services/geo_service_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe 'Geolocation API' do
   it 'returns list of user location' do
     parsed_json = GeoService.get_location
+
     expect(parsed_json).to be_a Hash
+    expect(parsed_json[:area][:name]).to be_a String
+    expect(parsed_json[:city][:name]).to be_a String
   end
 end
